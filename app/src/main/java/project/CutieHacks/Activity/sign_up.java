53267f3209d5d27dee83a99894fd08aa09646f40
peeyon <techiepeeyon@gmail.com>
2020-11-08 00:00:14 +0530

    added  signup xml page

--- app/src/main/java/project/CutieHacks/Activity/sign_up.java
+++ app/src/main/java/project/CutieHacks/Activity/sign_up.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class signup extends AppCompatActivity {
+public class sign_up extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signup);
+        setContentView(R.layout.activity_sign_up);
     }
 }
