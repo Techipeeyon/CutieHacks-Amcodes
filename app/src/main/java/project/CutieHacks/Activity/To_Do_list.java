90ed0874b03865d0cb238b4e034db5a4c7342cc6
peeyon <techiepeeyon@gmail.com>
2020-11-08 06:36:53 +0530

    completed dashboard, and other four modules

---
+++ app/src/main/java/project/CutieHacks/Activity/To_Do_list.java
@@ -0,0 +1,14 @@
+package project.CutieHacks.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class To_Do_list extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_to__do_list);
+    }
+}
